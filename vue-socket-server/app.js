da817e08cee820d25e0772e8a6f7fe044258f064
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2024-01-11 23:30:10 -0600

    init socket in server

--- vue-socket-server/app.js
+++ vue-socket-server/app.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const app = require('express')()
 const http = require('http').Server(app);
-const io = require('socketi.io')(http);
+const io = require('socket.io')(http);
 
 app.get('/',(req,res) => {
     res.send('socket.io...');
 });
+
+io.on('connection',(socket)=>{
+    console.log("connect socket");
+    socket.on('increment',(counter)=>{
+        console.log( "increment" );
+        io.sockets.emit('COUNTER_INCREMENT', counter + 1);
+    });
+    socket.on('decrement',(counter)=>{
+        console.log( "decrement" );
+        io.sockets.emit('COUNTER_DECREMENT', counter - 1);
+    });
+});
+
+http.listen(5000,()=>{
+    console.log( "listening on *:5000" );
+});
+
+module.exports = app;
